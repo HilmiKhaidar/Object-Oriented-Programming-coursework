be450683253402b66660691385322815e9e3c23a
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-05-19 15:13:22 -0700

    Mid-term exam questions 2

---
+++ UTS2/NO2/Catatan/Himpunan.java
@@ -0,0 +1,24 @@
+class Himpunan {
+    private int id;
+    private String nama;
+    
+
+    Himpunan() {
+    }
+
+    public void setId(int var1) {
+        this.id = var1;
+    }
+
+    public void setNama(String var1) {
+        this.nama = var1;
+    }
+
+    public int getId()  {
+        return this.id;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+}
